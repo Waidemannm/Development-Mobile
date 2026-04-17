bc48bf925b22deebd29411035fafb84deece15d6
Waidemannm <mjrmolinillo@icloud.com>
2026-04-16 22:15:21 -0300

    buscando dados no AsyncStorage para tela de perfil

--- Cps/cp2/app/perfil.tsx
+++ Cps/cp2/app/perfil.tsx
@@ -1,10 +1,90 @@
 import { SafeAreaView } from "react-native-safe-area-context";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function Perfil(){
+
+    const [nome, setNome] = useState("");
+    const [rm, setRm] = useState("");
+    const [telefone, setTelefone]= useState("");
+    const [cpf, setCpf] = useState("");
+
+    const [usuario, setUsuario] = useState({});
+
+    useEffect(() => {
+        BuscarDados();
+    }, [BuscarDados()]) 
+
+    async function BuscarDados(){
+        let data = await AsyncStorage.getItem("USUARIO");
+        if(data != null){
+            setUsuario(JSON.parse(data));
+            setNome(JSON.parse(data).nomeUser)
+            setRm(JSON.parse(data).rmUser)
+            setTelefone(JSON.parse(data).telefoneUser)
+            setCpf(JSON.parse(data).cpfUser)
+        }
+    }
+    
     return(
-        <SafeAreaView>
-        
+        <SafeAreaView style={styles.container}>
+            <Image style={styles.img} source={require("../assets/foto.jpeg")}/>
+            <Text style={styles.title}>Bem-vindo {nome}</Text>
+    
+            <Text style={styles.description}>
+                Aqui estão suas informações da conta
+            </Text>
+    
+            <Text style={styles.infos}>
+                RM: {rm}
+            </Text>
+            <Text style={styles.infos}>
+                Telefone: {telefone}
+            </Text>
+            <Text style={styles.infos}>
+                CPF: {cpf}
+            </Text>
         </SafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#7e6ef1",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    color: "#1e1e1e",
+  },
+
+  subtitle: {
+    fontSize: 18,
+    fontWeight: "600",
+    color: "#333",
+  },
+
+  description: {
+    fontSize: 14,
+    textAlign: "center",
+    color: "#444",
+  },
+  infos: {
+    fontSize: 14,
+    textAlign: "center",
+    color: "#141313",
+  },
+  img:{
+    borderWidth:2,
+    borderColor: "#c1e61f",
+    height:200,
+    width:200,
+    borderRadius: 100,
+    marginBottom: 20
+  },
+});
